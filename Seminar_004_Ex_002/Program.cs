107411cac8963f2877327914a615b8c32537646f
IgorMarokhin <lawyer.mii222@gmail.com>
2023-01-13 13:48:46 +0300

    Добавлено решение Задачи 26

---
+++ Seminar_004_Ex_002/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход
+// число и выдаёт количество цифр в числе
+
+Console.Clear();
+int num = GetNumberFromUser("Введите целое число: ","Ошибка ввода!");
+int digNumbers = GetDigNumbers(num);
+Console.WriteLine($"{num} -> {digNumbers}");
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+    Console.WriteLine(errorMessage);
+    }
+}
+
+int GetDigNumbers(int number)
+{
+    int dig = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        dig++;
+    }
+    return dig;
+}
