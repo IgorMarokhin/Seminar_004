1d5eb266440b1c84e373257aee269267f05e51e3
IgorMarokhin <lawyer.mii222@gmail.com>
2023-01-13 18:58:38 +0300

    Добавлено решение Задачи 30

---
+++ Seminar_004_Ex_004/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов,
+// заполненный нулями и единицами в случайном порядке.
+
+Console.Clear();
+GetArrayNumbers();
+
+void GetArrayNumbers()
+{
+    Console.Write("[");
+    for (int i = 0; i < 8; i++)
+    {
+        int number = new Random().Next(0,2);
+        Console.Write($"{number}");
+        if (i < 7) Console.Write(",");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
