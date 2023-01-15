a6682f7332c730747c8ae1177da209b76db58b8d
IgorMarokhin <lawyer.mii222@gmail.com>
2023-01-15 11:49:21 +0300

    Домашнее задание. Дбавлено решение Задачи 29

---
+++ S_004_HW_003_Task29/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт
+// массив из 8 элементов и выводит их на экран.
+
+Console.Clear();
+GetArrayNumbers();
+
+void GetArrayNumbers()
+{
+    Console.Write("[");
+    for (int i = 0; i < 8; i++)
+    {
+        int number = new Random().Next();
+        Console.Write($"{number}");
+        if (i < 7) Console.Write(",");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
